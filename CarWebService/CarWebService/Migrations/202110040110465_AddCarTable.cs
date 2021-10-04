d22f80f390d6d71cb349d962eb337e4941e7f1d5
Alex Aquino <aaquino1223@gmail.com>
2021-10-03 21:12:16 -0400

    Added Car Migration

---
+++ CarWebService/CarWebService/Migrations/202110040110465_AddCarTable.cs
@@ -0,0 +1,28 @@
+﻿namespace CarWebService.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddCarTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Cars",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        Name = c.String(nullable: false),
+                        Color = c.String(nullable: false),
+                        YearMade = c.String(nullable: false, maxLength: 4),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Cars");
+        }
+    }
+}
---
+++ CarWebService/CarWebService/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace CarWebService.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CarWebService.Models.CarDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(CarWebService.Models.CarDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
